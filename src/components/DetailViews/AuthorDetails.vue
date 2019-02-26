40a074311a36e1fab7fecad6d2f974493b3dbd73
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-02-26 11:04:29 +0100

    add more details for author view

--- src/components/DetailViews/AuthorDetails.vue
+++ src/components/DetailViews/AuthorDetails.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="author-container">
     <h1> {{author}} </h1>
-    <div class="co-authors"></div>
-    <div class="author-index" v-for="index in result">
-      {{index}}
-    </div>
+    <dl> 
+      <dt>index</dt>
+      <dd v-for="index in result" :key="index.idx"> {{index.idx}}: {{index['count(*)']}} </dd>
+      <dt>co-authors</dt>
+      <dd v-for="facet in coAuthors" :key="facet.name"> {{facet.name}}: {{facet.count}} </dd>
+      <dt>journals</dt>
+      <dd v-for="facet in journals" :key="facet.name">{{facet.name}}: {{facet.count}}</dd>
+      <dt>keywords</dt>
+      <dd v-for="facet in keywords" :key="facet.name">{{facet.name}}: {{facet.count}}</dd>
+    </dl>    
   </div>
 </template>
 <script lang="ts">
@@ -12,54 +18,115 @@ interface FacetDetail {
   name: string;
   count: number;
 }
+interface AuthorIndex {
+  idx: number;
+  'count(*)': number;
+}
 import Vue from 'vue';
+import {FacetResponse, FacetFields} from '@/plugins/vue-solr/lib/responses/FacetResponse';
+
+function* gen_pairs(arr: any[]) {
+  let name: string;
+  let count: number;
+  while (arr.length) {
+    [name, count, ...arr] = arr;
+    if (0 === count) {
+      continue;
+    }
+    if (name === '') {
+      continue;
+    }
+    yield { name, count };
+  }
+}
+
 export default Vue.extend({
   props: {
     author: {
       type: String,
       required: true,
     },
+    collection: {
+      type: String,
+      default: 's2',
+    },
   },
   data: () => ({
-    result: new Array<FacetDetail>(),
-    coAuthors: [],
-    journals: [],
-    keywords: [],
+    facets: ['author', 'journal', 'venue', 'year', 'keywords'],
+    result: new Array<AuthorIndex>(),
+    facetResponse: {} as FacetResponse,
+    coAuthors: new Array<FacetDetail>(),
+    journals: new Array<FacetDetail>(),
+    keywords: new Array<FacetDetail>(),
   }),
   methods: {
-    authorIndexGroup(author: string) {
-      const collection = 's2';
+    authorIndexGroup() {
+      const author = this.author;
       const idx = 'idx';
       const escaped = author.replace(/ /g, '\\ ');
-      const search = `search(${collection}, q="author:${escaped}", fl="author, id", sort="id desc", qt=/export)`;
+      const search = `search(${this.collection}, q="author:${escaped}", fl="author, id", sort="id desc", qt=/export)`;
       const select = `select(${search}, indexOf(author, "${author}") as ${idx})`;
       const sort = `sort(${select}, by="${idx} asc")`;
       const rollup = `rollup(${sort}, over="${idx}", count(*))`;
       console.log(rollup);
-      this.$solr.pass_through_solr.get(`/${collection}/stream`, {expr: rollup})
+      this.$solr.pass_through_solr.get(`/${this.collection}/stream`, {expr: rollup})
         .then((data: any) => {
           console.log(data);
-          this.result = data['result-set'].docs;
-        });
+          this.result = data['result-set'].docs.slice(0, -1);
+        }).catch(console.error);
+    },
+    getFacets() {
+      const escaped = this.author.replace(/ /g, '\\ ');
+      const payload = { params: {
+            'debug': false,
+            'q': `author:${escaped}`,
+            'facet': 'on',
+            'rows': 0,
+            'facet.field': this.facets,
+            }};
+      this.$solr.pass_through.get('/collections/s2/select', payload)
+        .then((d: any) => {
+          const tmp = d as FacetResponse;
+          this.facetResponse = d;
+          const flds = tmp.facet_counts.facet_fields;
+          this.coAuthors = [...gen_pairs(flds.author)];
+          this.journals = [...gen_pairs(flds.journal)];
+          this.keywords = [...gen_pairs(flds.keywords)];
+        }).catch(console.error);
+    },
+    update() {
+      this.authorIndexGroup();
+      this.getFacets();
     },
   },
   watch: {
     author() {
-      this.authorIndexGroup(this.author);
+      this.update();
     },
   },
   mounted() {
-    this.authorIndexGroup(this.author);
+    this.update();
   },
 });
 </script>
 
 <style scoped>
 .author-container {
- position: fixed;
- display: block;
- top: 0;
- right: 0;
- width: 25%;
+  position: fixed;
+  overflow:auto;
+  display: block;
+  top: 0;
+  right: 0;
+  width: 25%;
+  height:100%;
+}
+dt {
+  background-color: black;
+  color: white;
+  padding: 10px;
+  position: sticky;
+  top: 0;
+  left: 0;
+  margin: 1em 0;
 }
 </style>
---
+++ src/plugins/vue-solr/lib/responses/FacetResponse.ts
@@ -0,0 +1,42 @@
+export interface FacetResponse {
+  responseHeader: ResponseHeader;
+  response: Response;
+  facet_counts: FacetCounts;
+}
+
+export interface FacetCounts {
+  facet_queries: Facet;
+  facet_fields: FacetFields;
+  facet_ranges: Facet;
+  facet_intervals: Facet;
+  facet_heatmaps: Facet;
+}
+
+export interface FacetFields {
+  author: Array<number | string>;
+  journal: Array<number | string>;
+  venue: Array<number | string>;
+  year: Array<number | string>;
+  keywords: Array<number | string>;
+}
+
+export interface Facet {
+}
+
+export interface Response {
+  numFound: number;
+  start: number;
+  maxScore: number;
+  docs: any[];
+}
+
+export interface ResponseHeader {
+  zkConnected: boolean;
+  status: number;
+  QTime: number;
+  params: Params;
+}
+
+export interface Params {
+  json: string;
+}
