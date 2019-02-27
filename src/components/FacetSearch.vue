1b649d0d860e1adc38a90f8e5d1133f481034975
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-02-27 13:45:31 +0100

    clean up

--- src/components/FacetSearch.vue
+++ src/components/FacetSearch.vue
@@ -3,104 +3,57 @@
       <input id="search-input" v-model="query" @keyup.enter="submitSearch" type="text" title="search text"/>
       <collection-select @change="collection = $event"/>
       <input id="search-submit" @click="submitSearch" type="button" value="search"/>
-      <solr-facet-search-result v-if="numFound" :response="result"/>
+      <facet-search-result v-if="result" :response="result"/>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import SolrFacetSearchResult from './FacetSearch/FacetSearchResult.vue';
+import FacetSearchResult from './FacetSearch/FacetSearchResult.vue';
 import CollectionSelect from './CollectionSelect.vue';
 
 export default Vue.extend({
   name: 'FacetSearch',
-  components: { SolrFacetSearchResult, CollectionSelect },
+  components: { FacetSearchResult, CollectionSelect },
   data: () => ({
     query: 'author:*Ropinski*',
-    lastQuery: '',
     collection: 's2',
-    result: {},
-    start: 0,
-    docs: new Array<any>(),
-    facets: ['author', 'journal', 'venue', 'year', 'keywords'],
-    pageDocs: new Map<number, any[]>(),
-    currentPage: '1',
-    numFound: 0,
-    rows: 10,
+    result: null,
   }),
-  watch: {
-    currentPage() {
-      const page = this.activePage;
-      const docs = this.pageDocs.get(page);
-      if (docs) {
-        this.docs = docs;
-        return;
-      }
-      this.pageDocs.set(page, []);
-      this.getPageData(page);
-    },
-  },
   methods: {
     log(content: any) {
       this.$store.dispatch('log/log', content);
     },
     submitSearch() {
-      this.pageDocs = new Map();
-      this.lastQuery = this.query;
-      // this.start = 0;
-      this.numFound = 0;
       const payload = { params: {
-            'debug': false,
-            'q': this.lastQuery,
-            'facet': 'on',
-            'rows': 0,
-            'facet.field': this.facets,
-            }};
-      // const payload = {params: {q: this.lastQuery, rows: this.rows, start: this.start}};
+        'q': this.query,
+        'facet': 'on',
+        'rows': 0,
+        'facet.field': this.facets,
+        'defType': 'edismax',
+        'qf': 'suggest_lower^10 suggest_ngram',
+        'q.op': 'AND',
+        }};
       this.$solr.select({collection: this.collection, payload})
         .then((d: any) => {
           this.result = d;
-          this.docs = d.facet_counts;
-          this.pageDocs.set(1, d.facet_counts);
-          this.numFound = d.response.numFound;
         }).catch(console.error);
-    },
-    getPageData(page: number) {
-      const start = (page - 1) * this.rows;
-      const payload = {params: {q: this.lastQuery, rows: this.rows, start}};
-      this.$solr.select({collection: this.collection, payload})
-        .then((d: any) => {
-          this.result = d;
-          this.pageDocs.set(page, d.response.docs);
-          this.numFound = d.response.numFound;
-          if (this.activePage === page) {
-            this.docs = d.response.docs;
-          }
-        }).catch(console.error);
-    },
-    pageChanged() {
-      const page = this.activePage;
-      if (this.pageDocs.has(page)) {
-        const val = this.pageDocs.get(page);
-        if (val) {
-          this.docs = val;
-          return;
-        }
-      }
-      this.pageDocs.set(page, []);
-      this.getPageData(this.activePage);
     },
   },
   computed: {
-    pageCount(): number {
-      if ( this.numFound === 0) {
-        return 0;
+    facets() {
+      if ('s2' === this.collection) {
+        return ['author', 'journal', 'venue', 'year', 'keywords'];
       }
-      const num = this.numFound / this.rows;
-      return Math.floor(num) + 1;
+      if ('dblp' === this.collection) {
+        return ['author', 'journal', 'booktitle', 'year'];
+      }
+      return [];
     },
-    activePage(): number {
-      return Number.parseInt(this.currentPage, 10);
+  },
+  watch: {
+    collection() {
+      this.submitSearch();
     },
   },
 });
