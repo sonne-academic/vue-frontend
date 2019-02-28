a441e37596cc2ce004d9743e8dd1cf91bdadec73
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-02-28 16:32:53 +0100

    revert worker

--- src/plugins/vue-solr/lib/solr.worker.ts
+++ src/plugins/vue-solr/lib/solr.worker.ts
@@ -1,7 +1,6 @@
 import RpcHandler from './RpcHandler';
 const context: Worker = self as any;
-// const addr = process.env.VUE_APP_SONNE_HOST + '/ws/rpc/solr/';
-const addr = 'ws://localhost:8000/ws/rpc/solr/';
+const addr = process.env.VUE_APP_SONNE_HOST + '/ws/rpc/solr/';
 const socket = new RpcHandler(addr, context);
 context.addEventListener('message', (event) => {
   socket.send(JSON.stringify(event.data));
