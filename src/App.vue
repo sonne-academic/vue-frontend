7895297dba8b14e5f316c574caa3d1073c8a2698
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-02-28 16:30:12 +0100

    use cy component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <sidebar id="side" @starttree="startTree"/>
+    <cy id='cy'/>
     <!-- <three-tree-view name="container" :rootid="rootid"/> -->
     <global-log id="log"/>
   </div>
@@ -12,11 +13,13 @@ import GlobalLog from './components/GlobalLog.vue';
 import SearchBox from './components/SearchBox.vue';
 // import ThreeTreeView from './components/ThreeTreeView.vue';
 import Sidebar from './components/Sidebar.vue';
+import cy from './components/Cytoscape.vue';
 
 export default Vue.extend({
   name: 'app',
   components: {
     GlobalLog,
+   cy,
     // ThreeTreeView: () => import('./components/ThreeTreeView.vue'),
     Sidebar,
   },
@@ -54,5 +57,11 @@ body {
   border: 1px solid #ccc;
   width: 33%;
 }
-
+#cy {
+  position: absolute;
+  width: 33%;
+  height: 100%;
+  left: 33%;
+  top: 0;
+}
 </style>
---
+++ src/components/Cytoscape.vue
@@ -0,0 +1,35 @@
+<template>
+  <div ref="cy"/>
+</template>
+
+<script lang="ts">
+const style = [ // the stylesheet for the graph
+  {
+    selector: 'node',
+    style: {
+      'background-color': '#666',
+      'label': 'data(id)',
+    },
+  },
+
+  {
+    selector: 'edge',
+    style: {
+      'width': 3,
+      'line-color': '#ccc',
+      'target-arrow-color': '#ccc',
+      'target-arrow-shape': 'triangle',
+    },
+  },
+];
+import Vue from 'vue';
+export default Vue.extend({
+  mounted() {
+    const r = this.$refs;
+    this.$cy.setConfig({style});
+    this.$cy.instance.then((cy) => {
+      cy.mount(r.cy as Element);
+    });
+  },
+});
+</script>
