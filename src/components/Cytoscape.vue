9a2d99f19eea48896375c09d99951dad110cefd5
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-03-02 16:18:25 +0100

    add cxtmenu

--- src/components/Cytoscape.vue
+++ src/components/Cytoscape.vue
@@ -16,68 +16,50 @@ export default Vue.extend({
       mod.use(cxtmenu);
     },
   },
-  {
-    selector: 'node[kind=\"search\"]',
-    style: {
-      'background-color': '#eee',
-      'label': 'data(query)',
-      'background-image': '/close.min.svg',
-      'background-fit': 'contain',
-    },
-  },
-  {
-    selector: 'node[kind=\"facet\"][field=\"author\"]',
-    style: {
-      'background-color': '#eee',
-      'label': 'data(value)',
-      'background-image': '/author.svg',
-      'background-width': '75%',
-      'background-height': '75%',
-      // 'background-fit': 'contain',
-    },
-  },
-  {
-    selector: 'node[kind=\"facet\"][field=\"journal\"]',
-    style: {
-      'background-color': '#eee',
-      'label': 'data(value)',
-      // 'background-image': '/author.svg',
-      // 'background-width': '75%',
-      // 'background-height': '75%',
-      // 'background-fit': 'contain',
-    },
-  },
-
-  {
-    selector: 'edge',
-    style: {
-      'width': 3,
-      'line-color': '#eee',
-      'target-arrow-color': '#ccc',
-      'target-arrow-shape': 'triangle',
-    },
-  },
-],
-};
-import Vue from 'vue';
-import cfg from './Cytoscape';
-export default Vue.extend({
   mounted() {
     const r = this.$refs;
-    this.$cy.setConfig(config);
+    this.$cy.setConfig(config, this.preconfig);
     this.$cy.instance.then((cy) => {
       console.log('mounting');
       cy.mount(r.cy as Element);
+      this.menu = cy.cxtmenu({
+        selector: 'node, edge',
+        openMenuEvents: 'cxttapstart taphold',
+        commands: [
+          {
+            content: 'remove',
+            select(ele) {
+              console.log( ele.id() );
+              ele.remove();
+            },
+          },
+
+          {
+            content: 'name',
+            select(ele) {
+              console.log( ele.data('name') );
+            },
+            enabled: false,
+          },
+
+          {
+            content: 'Text',
+            select(ele) {
+              console.log( ele.position() );
+            },
+          },
+        ],
+      });
       for (const [eventType, callback] of Object.entries(this.$listeners)) {
        cy.on(eventType, (event) => (callback as cytoscape.EventHandler)(event));
       }
-
     });
   },
   beforeDestroy() {
     this.$cy.instance.then((cy) => {
       console.log('umounting');
       cy.unmount();
+      this.menu.destroy();
     });
   },
 });
