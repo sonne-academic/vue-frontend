fb143906740a85d265068d79741859fb749edb3f
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-03-05 11:30:40 +0100

    remove graph store again

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import log from './modules/log';
-import navgraph from './modules/navgraph';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
     log,
-    navgraph,
   },
   // plugins: [createSolrCommandPlugin()],
   strict: true,
@@ -16,14 +14,12 @@ const store = new Vuex.Store({
 
 if (module.hot) {
   // accept actions and mutations as hot modules
-  module.hot.accept(['./modules/log', './modules/navgraph'], () => {
+  module.hot.accept(['./modules/log'], () => {
     const newLog = require('./modules/log').default;
-    const newNG = require('./modules/navgraph').default;
     // swap in the new modules and mutations
     store.hotUpdate({
       modules: {
         log: newLog,
-        navgraph: newNG,
       },
     });
   });
