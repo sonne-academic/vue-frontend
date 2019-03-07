c3d85ed63cf4ee93d474028004e50ef985d89065
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-03-07 17:38:51 +0100

    types for dagre

--- src/types/cytoscape/index.d.ts
+++ src/types/cytoscape/index.d.ts
@@ -1,8 +1,26 @@
-import cytoscape from 'cytoscape';
+import cytoscape, { LayoutOptions, Layouts } from 'cytoscape';
 import * as cm from 'cytoscape-cxtmenu';
+import * as cd from 'cytoscape-dagre';
+
 
 declare module 'cytoscape' {
+  type LayoutExtensions = LayoutOptions|cd.DagreLayoutOptions;
   export interface Core {
     cxtmenu: (args: cm.MenuArgs) => cm.Destructor;
   }
+  // export interface CytoscapeOptions {
+  //   layout?: LayoutOptions | cd.DagreLayoutOptions;
+  // }
+  export interface CoreLayout {
+    layout(layout: LayoutExtensions): Layouts;
+    makeLayout(options: LayoutExtensions): Layouts;
+    createLayout(options: LayoutExtensions): Layouts;
+  }
+  interface CollectionLayout {
+    layout(options: LayoutExtensions): Layouts;
+    makeLayout(options: LayoutExtensions): Layouts;
+    createLayout(options: LayoutExtensions): Layouts;
+}
+
+
 }
