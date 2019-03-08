887bb11832bbf43000f8591fe78e6e440f7cc957
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-03-08 11:26:50 +0100

    use collection information on emitters

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -2,11 +2,11 @@
   <div>
     <h4><span>{{doc.title}}</span></h4>
     <div>
-      <i v-if="doc.journal"><simple-emitter field="journal" :name="doc.journal"/></i>
-      <i v-else-if="doc.venue"><simple-emitter field="venue" :name="doc.venue"/></i>
+      <i v-if="doc.journal"><simple-emitter :collection="collection" field="journal" :name="doc.journal"/></i>
+      <i v-else-if="doc.venue"><simple-emitter :collection="collection" field="venue" :name="doc.venue"/></i>
     </div>
     {{doc.year}} - <span v-for="(author, index) in doc.author" :key="author">
-      <simple-emitter field="author" :name="author"/><span v-if="index+1 < doc.author.length">, </span></span>
+      <simple-emitter :collection="collection" field="author" :name="author"/><span v-if="index+1 < doc.author.length">, </span></span>
     <!-- <details v-if="doc.keywords">
       <summary>keywords</summary>
         {{ doc.keywords.join(', ') }}
