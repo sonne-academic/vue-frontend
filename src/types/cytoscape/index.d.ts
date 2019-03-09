14a40daa376c6c9e0d4f68fb7605b57f6cc9ffdf
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-03-09 11:31:26 +0100

    add compound extension

--- src/types/cytoscape/index.d.ts
+++ src/types/cytoscape/index.d.ts
@@ -1,12 +1,14 @@
 import cytoscape, { LayoutOptions, Layouts } from 'cytoscape';
 import * as cm from 'cytoscape-cxtmenu';
 import * as cd from 'cytoscape-dagre';
+import * as cdnd from 'cytoscape-compound-drag-and-drop'
 
 
 declare module 'cytoscape' {
   type LayoutExtensions = LayoutOptions|cd.DagreLayoutOptions;
   export interface Core {
     cxtmenu: (args: cm.MenuArgs) => cm.Destructor;
+    compoundDragAndDrop: (options: cdnd.CompoundOptions) => cdnd.CompundObject;
   }
   // export interface CytoscapeOptions {
   //   layout?: LayoutOptions | cd.DagreLayoutOptions;
