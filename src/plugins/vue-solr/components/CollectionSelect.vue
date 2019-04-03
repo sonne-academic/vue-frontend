dbec64080e009b51482c5b83a982c3bdbd465343
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-04-03 15:51:30 +0200

    collection select accepts default value

--- src/plugins/vue-solr/components/CollectionSelect.vue
+++ src/plugins/vue-solr/components/CollectionSelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <select @change="$emit('change',$event.target.value)">
+  <select ref="select" @change="$emit('change',$event.target.value)">
     <option 
       v-for="c in $solr.collections" :key="c" 
       :value="c"
@@ -12,8 +12,31 @@
 import Vue from 'vue';
 export default Vue.extend({
   name: 'CollectionSelect',
+  props: {
+    initCollection: {
+      type: String,
+      default: '',
+    },
+  },
   mounted() {
-    this.$emit('change', this.$solr.collections[0]);
+    if (this.initCollection === '') {
+      this.$emit('change', this.$solr.collections[0]);
+    } else {
+      for (const o of this.select)  {
+        if (o.nodeName === 'OPTION') {
+          const opt = o as HTMLOptionElement;
+          if (opt.value === this.initCollection) {
+            opt.selected = true;
+          }
+        }
+      }
+      this.$emit('change', this.initCollection);
+    }
+  },
+  computed: {
+    select(): HTMLSelectElement {
+      return this.$refs.select as HTMLSelectElement;
+    },
   },
 });
 </script>
