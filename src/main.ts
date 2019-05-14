99bc9c0fa2849d82f8198b06b0913e405605064f
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-05-14 12:02:38 +0200

    use mag as default

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import store from './store';
 import solr from './plugins/vue-solr/plugin';
 import cy from './plugins/vue-cy/plugin';
 const collections = new Map<string, string[]>();
-collections.set('s2', ['author', 'year', 'keywords', 'journal', 'venue']);
 collections.set('mag', ['author', 'year', 'journal', 'venue']);
+collections.set('s2', ['author', 'year', 'keywords', 'journal', 'venue']);
 collections.set('dblp', ['author', 'year', 'journal', 'booktitle']);
 Vue.use(solr, {
   collections,
