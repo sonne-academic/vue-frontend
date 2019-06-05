aa62799d735046b951f0e6704ec01062640df487
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-06-05 15:18:46 +0200

    try searcher, to make code more obvious

--- src/plugins/vue-solr/lib/Searcher.ts
+++ src/plugins/vue-solr/lib/Searcher.ts
@@ -1,45 +1,73 @@
 import WorkerWrapper from './WorkerWrapper';
+import { RpcResult } from './RpcInterface';
+
+type result = RpcResult<{response: { numFound: number, docs: any[], start: number }}>;
 
 export class Searcher {
   private query: string;
   private collection: string;
+  private sort: string;
   private solr: WorkerWrapper;
   private rows: number = 10;
-  private responses: Map<number, Promise<any>> = new Map();
+  private responses: Map<number, Promise<result>> = new Map();
+  private responseComplete: Map<number, boolean> = new Map();
+  private facetResponses: Map<string, Promise<any>> = new Map();
 
-  constructor($solr: WorkerWrapper, $query: string, $collection: string) {
+  constructor($solr: WorkerWrapper, $query: string, $collection: string, $sort: string) {
     this.query = $query;
     this.collection = $collection;
+    this.sort = $sort;
     this.solr = $solr;
   }
 
-  public getPage(num: number): Promise<any> {
-    const p = this.responses.get(num);
-    if (p) {
-      return p;
-    }
+  public getPage(num: number) {
     return this.search(num);
+  }
+
+  public pageLoadFinished(page: number): boolean {
+    const r = this.responseComplete.get(page);
+    return !!r;
   }
 
   public async pageCount() {
     const resolved = await Promise.race(this.responses.values());
-    return resolved.response.numFound;
+    return resolved.result.response.numFound;
   }
 
-  private async search(page: number) {
-    const start = (page - 1) * this.rows;
-    const payload = {params: {
+  public get payload() {
+    return {
       'q': this.query,
       'rows': this.rows,
-      'debug': 'query',
-      start,
+      'sort': this.sort,
       'defType': 'edismax',
-      // 'fl': 'title',
       'qf': 'suggest_lower^10 suggest_ngram',
       'q.op': 'AND',
-      // 'omitHeader': 'true',
-      'sort': `year desc`}};
-    const p = this.solr.select({collection: this.collection, payload});
+      'debug': 'query',
+    };
+  }
+
+  public async getFacet(field: string) {
+    const oldPromise = this.facetResponses.get(field);
+    if (undefined !== oldPromise) {
+      return oldPromise;
+    }
+    const rows = 0;
+    const facet = 'on';
+    const payload = {...this.payload, rows, facet, 'facet.field': field};
+    const newPromise = this.solr.select({collection: this.collection, payload: {params: payload}});
+    this.facetResponses.set(field, newPromise);
+    return newPromise;
+  }
+
+  private search(page: number) {
+    const oldPromise = this.responses.get(page);
+    if (undefined !== oldPromise) {
+      return oldPromise;
+    }
+    const start = (page - 1) * this.rows;
+    const payload = {...this.payload, start};
+    const p = this.solr.select({collection: this.collection, payload: {params: payload}});
+    p.then((x) => this.responseComplete.set(page, true));
     this.responses.set(page, p);
     return p;
   }
