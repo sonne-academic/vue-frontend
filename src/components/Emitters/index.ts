e9ef5d316d3209d2a2713657751b0e7a9a8c4ef9
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-06-05 15:49:01 +0200

    add author list to index

--- src/components/Emitters/index.ts
+++ src/components/Emitters/index.ts
@@ -2,5 +2,6 @@ import SimpleEmitter from './Simple.vue';
 import PaperEmitter from './Paper.vue';
 import SimpleFacetBox from './SimpleFacetBox.vue';
 import SearchResult from './SearchResult.vue';
+import AuthorList from './AuthorList.vue';
 
-export {SimpleEmitter, SimpleFacetBox, SearchResult, PaperEmitter};
+export {SimpleEmitter, SimpleFacetBox, SearchResult, PaperEmitter, AuthorList};
